f73bdb409cf3b9beeb77432018a213bd889a1f7c
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-11-19 21:28:00 -0600

    Update Question.js

--- js/Question.js
+++ js/Question.js
@@ -2,9 +2,9 @@ class Question {
 
   constructor() {
     this.title = createElement('h1')
-    this.input1 = createInput("Enter Your Name Here....");
-    this.input2 = createInput("Enter Correct Option No..");
-    this.button = createButton('Submit');
+    this.input1 = createInput("Ingresa tu nombre aquí....");
+    this.input2 = createInput("Ingresa el correcto número de opción..");
+    this.button = createButton('Enviar');
     this.question = createElement('h3');
     this.option1 = createElement('h4');
     this.option2 = createElement('h4');
@@ -23,15 +23,15 @@ class Question {
     this.title.html("MyQuiz Game");
     this.title.position(350, 0);
 
-    this.question.html("Question:- What starts and ends with the letter ‘E’, but has only one letter? " );
+    this.question.html("Pregunta:- Un señor muy gordito y coloradito, no toma café pues siempre toma té. " );
     this.question.position(150, 80);
-    this.option1.html("1: Everyone " );
+    this.option1.html("1: Tapete " );
     this.option1.position(150, 100);
-    this.option2.html("2: Envelope" );
+    this.option2.html("2: Tomate" );
     this.option2.position(150, 120);
-    this.option3.html("3: Estimate " );
+    this.option3.html("3: Taquero " );
     this.option3.position(150, 140);
-    this.option4.html("4: Example" );
+    this.option4.html("4: Tenedor" );
     this.option4.position(150, 160);
 
     this.input1.position(150, 230);
